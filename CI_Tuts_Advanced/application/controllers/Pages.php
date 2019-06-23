fb318bde8ae1e8b0a6b347656354b26f4178b98a
latimercaleb <nohitokaze@gmail.com>
2019-06-23 04:30:45 -0400

    Completion of advanced section, studying HMVC now

--- CI_Tuts_Advanced/application/controllers/Pages.php
+++ CI_Tuts_Advanced/application/controllers/Pages.php
@@ -14,7 +14,10 @@
                   // $this->load->view('Pages/home', $this->data);
                   // $data = $this->page->find(); // For all posts
                   // $data = $this->page->find(3); // For single post
-                  $data = $this->page->find(array(3,4)); // For more than one post
+                  // $data = $this->page->find(array(3,4)); // For more than one post
+                  // $data = $this->page->find_by('title','Fifth Post', NULL, TRUE); // Find by a title, could pass in an array with multiple params for a post and find that way as well
+                  // $data = $this->page->get_assoc(array(2,3));
+                  // $data = $this->page->to_assoc($data);
                   echo '<pre>';
                   print_r($data);
 
--- CI_Tuts_Advanced/application/core/MY_Model.php
+++ CI_Tuts_Advanced/application/core/MY_Model.php
@@ -28,5 +28,68 @@
                   return $this->db->get($this->table_name) ->$method();
             }
 
+            public function find_by($key, $val=FALSE, $orwhere=FALSE, $single=FALSE){
+                  if(!is_array($key)){
+                        $this->db->where(htmlentities($key), htmlentities($val));
+                  }else{
+                        $key = array_map('htmlentities', $key);
+                        $where_method = $orwhere == TRUE ? 'or_where' : 'where';
+                        $this->db->$where_method($key);
+                  }
+                  $single == FALSE || $this->db->limit(1);
+                  $method = $single ? 'row_array' : 'result_array';
+                  return $this->db->get($this->table_name) ->$method();
+            }
+
+            public function get_assoc($ids = FALSE){
+                  $result = $this->find($ids);
+                  if($ids != FALSE && !is_array($ids)){
+                        $result = array($result);
+                  }
+
+                  $data = $this->to_assoc($result);
+                  return $data;
+            }
+
+            public function to_assoc($result=array()){
+                  $data = array();
+                  if(count($result) > 0){
+                        foreach($result as $row){
+                              $tmp = array_values(array_slice($row,0,1));
+                              $data[$tmp[0]] = $row;
+                        }
+                  }
+                  return $data;
+            }
+
+            public function save($data, $id=FALSE){
+                  if($id == FALSE){
+                        // Insert case
+                        $this->db->set($data)->insert($this->table_name);
+                  }else{
+                        // Update case
+                        $filter = $this->primaryFilter;
+                        $this->db->set($data)->where($this->primary_key, $filter($id))->update($this->table_name);
+                  }
+                  return $id == FALSE ? $this->db->insert_id() : $id;
+            }
+
+            public function delete($ids){
+                  $filter = $this->primaryFilter;
+                  $ids = !is_array($ids) ? array($ids) : $ids;
+                  foreach($ids as $id){
+                        $id = $filter($id);
+                        if($id){
+                              $this->db->where($this->primary_key, $id) ->limit(1) -> delete($this->table_name);
+                        }
+                  }
+            }
+
+            public function delete_by($key, $value){
+                  if(empty($key)){
+                        return false;
+                  }
+                  $this->db->where(htmlentities($key), htmlentities($value))
+            }
       }
 ?>
